a344b77152fd5649a827c710cf5162a69caeed9f
valentinatp <valentina.toledoporma@gmail.com>
2017-05-08 21:27:03 -0300

    Creo la funcion 'calculadora'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-alert("Miau");
+function calculadora(){
+	var peso = parseInt(prompt("Ingresa tu peso"));
+	var altura = parseInt(prompt("Ingresa tu altura"))/100;
+	var calculo = (peso/(altura*altura)).toFixed(1);
+	return alert("Tu IMC es de: " + calculo);
+}
