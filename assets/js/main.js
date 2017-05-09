aa3817a1d989815d9c3db667923462af03d048b7
Maria Paz <maria.pazrojasc@gmail.com>
2017-05-08 21:34:14 -0300

    crear la funcion calcular indice de masa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-alert("Miau");
+function calcularImc(peso,estatura){
+  var peso = document.getElementById("peso").value;
+  var estatura = document.getElementById("estatura").value;
+  var altura  = parseInt(estatura)/100;
+  var imc = (peso / (altura*altura)).toFixed(0);
+
+  if (imc > 40) {
+    alert("Tienes un índice de: "+ imc + " Debes cuidar tu salud!");
+  } else alert("Tienes un índice de: "+ imc);
+
+
+}
